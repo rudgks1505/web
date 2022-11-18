417c7a0914dfde01d93681b8aa0f5c579467f5a4
rudgks1505 <95726555+rudgks1505@users.noreply.github.com>
2022-11-18 18:45:55 +0900

    nnn
    nnn

--- src/components/home/bbb.vue
+++ src/components/home/bbb.vue
@@ -299,7 +299,7 @@ export default {
         }
         timer = setTimeout(() => {
           vvv();
-        }, 600);
+        }, 500);
       });
       window.addEventListener("load", () => {
         home_back_con[0].style.top = "40%";
